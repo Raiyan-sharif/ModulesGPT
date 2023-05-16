3701cfdb9ca1119d9c485185065b2cfee73f8814
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2023-05-16 16:12:13 +0600

    Git ignore file

---
+++ ModulesGPT/ModulesGPT/ModulesGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModulesGPTApp.swift
+//  ModulesGPT
+//
+//  Created by BJIT on 16/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ModulesGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
